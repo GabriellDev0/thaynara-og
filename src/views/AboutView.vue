b6f595531ba403cc3f50319ef9609d06ba173c07
GabriellDev0 <gabrielldev0@gmail.com>
2022-06-09 11:22:59 -0300

    Iniciando Projeto, resetei para começar o projeto

--- src/views/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Testando o Action Github to firebase</h1>
-    <h1>Testando Nova Feature</h1>
-  </div>
-</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Bem-vindo ao Vue App"/>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
