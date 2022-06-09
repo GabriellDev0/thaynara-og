7615fff657db1339ee58067146786002a77b74bb
GabriellDev0 <gabrielldev0@gmail.com>
2022-06-09 12:00:20 -0300

    Configurações Iniciais do Projeto, reset CSS, variáveis que de CSS utilizadas no figma

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,40 @@
 </template>
 
 <style>
+:root{
+  --background-color: #FFFFFF;
+  --primary-color: #333333;
+  --secundary-color: #FF8585;
+  --terciary-color: #666666;
 
+  --font-sizeP: 1rem;
+  --font-sizeM: 1.125rem;
+
+  --font-bold: 700;
+  --font-regular: 400;
+  --font-light: 300;
+}
+
+
+*{
+  box-sizing: border-box;
+}
+body, ul, li, h1, h2, p{
+  padding: 0px;
+  margin: 0px;
+}
+ul{
+  list-style: none;
+}
+body{
+  font-family: "Poppins", Helvetica, Arial, sans-serif;
+}
+a{
+  text-decoration: none;
+}
+img{
+  max-width: 100%;
+  display: block;
+}
 
 </style>
